ec8b24d4dfe4cd1c9c940927dc5407d59ca5df56
Adrien Maranville <adrien.maranville@gmail.com>
2017-03-11 21:01:43 -0700

    Basic components added.

--- Bach to Basics/ViewController.swift
+++ Bach to Basics/ViewController.swift
@@ -7,12 +7,52 @@
 //
 
 import UIKit
+import AVFoundation
+
+
 
 class ViewController: UIViewController {
+    
+    var player = AVAudioPlayer()
 
+    @IBAction func btnPlay(_ sender: Any) {
+        player.play()
+    }
+    
+    @IBAction func btnStop(_ sender: Any) {
+        player.stop()
+        player.currentTime = 0.0;
+    }
+    
+    @IBAction func btnPause(_ sender: Any) {
+        player.pause()
+    }
+    
+    @IBOutlet weak var sliderTime: UISlider!
+    
+    @IBAction func sliderTimeMoved(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var sliderVolume: UISlider!
+    
+    @IBAction func sliderVolumeMoved(_ sender: Any) {
+        player.volume = sliderVolume.value
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let audioPath = Bundle.main.path(forResource: "bach", ofType: "mp3")
+        
+        do {
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+        }
+        catch {
+            
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
